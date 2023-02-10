a961416969471cf373fff09dc25be40d8ec65278
Abdullah Yousuf <23.abdullah.y@gmail.com>
2023-02-10 17:53:52 -0500

    worked on parsing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-mod node;
-use node::Node;
-use std::io;
+mod file_parser;
+use file_parser::FileParser;
+
+mod regex_parser;
+use regex_parser::RegExParser;
 
 use clap::Parser;
+
+mod node;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -16,40 +20,12 @@ fn main() {
 
     let args = Args::parse();
 
-    println!("{}", args.filename)    
+    println!("{}", args.filename);    
 
-    /*println!("Hello!");
+    // Open the file
+    FileParser::parse_file(&args.filename);
 
-    // Create the node
-    let mut parent = Node::new("S".to_string());
-
-    // Read input from command line
-    let mut line = String::new();
-    io::stdin().read_line(&mut line).expect("Failed to read line");
-
-    let mut prev = &mut parent;
-
-    // Loop through each character
-    for c in line.trim().chars() {
-        println!("Character {c}");
-
-        // Create a node for the character
-        let mut node = Box::new(Node::new(c.to_string()));
-
-        prev.child = Some(node);
-
-        //let mut inside = prev.child.unwrap();
-        prev = prev.child.as_mut().unwrap();
-
-    }
-
-    // Print out the contents of the linked list
-    prev = &mut parent;
-    while let Some(i) = &mut prev.child {
-        let node = i.as_mut();
-        print!("-{0}", node.data);
-        prev = node;//.child.as_mut().unwrap().as_mut();
-    }
-    print!("\n");*/
-
+    // Create a regex parser
+    let mut parser = RegExParser::new("hello");
+    parser.parse();
 }
