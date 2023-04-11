5c5c95913b13f5e06419889d74b865b53c29ac7d
Abdullah Yousuf <23.abdullah.y@gmail.com>
2023-04-11 19:26:32 -0400

    worked on grammar

--- src/grammar.rs
+++ src/grammar.rs
@@ -15,7 +15,8 @@ pub struct Production {
     pub prod: Vec<Symbol>,
 }
 
-struct LRItem {
+#[derive(Eq, Hash, PartialEq, Clone, Debug)]
+pub struct LRItem {
     production: *const Production,
     placeholder_index: usize,
 }
@@ -62,4 +63,51 @@ impl GrammarGenerator {
     }
 
     // Function to compute closure
+    pub fn get_closure(&self, set: &HashSet<LRItem>)
+    {
+        let mut changing = true;
+        while changing {
+            for item in set {
+                unsafe {
+                    if item.placeholder_index < (*item.production).prod.len() {
+                        // Get next symbol
+                        let next = &(*item.production).prod[item.placeholder_index];
+                        if !next.is_terminal {
+                            // Get rule for the nonterminal symbol
+                            let rule = match self.rules.get(next) {
+                                Some(val) => val,
+                                None => {
+                                    // Throw an error
+                                    todo!();
+                                }
+                            };
+
+                            // Go through the possible productions
+                            for prod in &rule.productions {
+                                // Get string after next symbol
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    fn get_next_symbols(prod: *const Production, index: usize) -> Vec<Symbol>
+    {   
+        let mut result = Vec::new();
+
+        unsafe {
+            let size = (*prod).prod.len();
+            for i in index..size
+            {
+                result.push((*prod).prod[i].clone());
+            }
+        }
+
+        // Add lookup symbol
+    
+
+        return result;
+    }
 }
